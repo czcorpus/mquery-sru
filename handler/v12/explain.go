c4c263ebdd10c8f7a307734023e240c103dcaa6a
Martin Zimandl <martin.zimandl@gmail.com>
2024-01-18 12:16:48 +0100

    v12 handler refactor

---
+++ handler/v12/explain.go
@@ -0,0 +1,66 @@
+// Copyright 2023 Martin Zimandl <martin.zimandl@gmail.com>
+// Copyright 2023 Tomas Machalek <tomas.machalek@gmail.com>
+// Copyright 2023 Institute of the Czech National Corpus,
+//                Faculty of Arts, Charles University
+//   This file is part of MQUERY.
+//
+//  MQUERY is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  MQUERY is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with MQUERY.  If not, see <https://www.gnu.org/licenses/>.
+
+package v12
+
+import (
+	"fcs/general"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (a *FCSSubHandlerV12) explain(ctx *gin.Context, fcsResponse *FCSResponse) int {
+	// check if all parameters are supported
+	for key, _ := range ctx.Request.URL.Query() {
+		if err := ExplainArg(key).Validate(); err != nil {
+			fcsResponse.General.Error = &general.FCSError{
+				Code:    general.CodeUnsupportedParameter,
+				Ident:   key,
+				Message: err.Error(),
+			}
+			return http.StatusBadRequest
+		}
+	}
+
+	// prepare response data
+	fcsResponse.Explain = FCSExplain{
+		ServerName:          a.serverInfo.ServerName,
+		ServerPort:          a.serverInfo.ServerPort,
+		Database:            a.serverInfo.Database,
+		DatabaseTitle:       a.serverInfo.DatabaseTitle,
+		DatabaseDescription: a.serverInfo.DatabaseDescription,
+	}
+	if ctx.Query(ExplainArgFCSEndpointDescription.String()) == "true" {
+		fcsResponse.Explain.ExtraResponseData = true
+		for corpusName, _ := range a.corporaConf.Resources {
+			fcsResponse.Explain.Resources = append(
+				fcsResponse.Explain.Resources,
+				FCSResourceInfo{
+					PID:         corpusName,
+					Title:       corpusName,
+					Description: "TODO",
+					URI:         "TODO",
+					Languages:   []string{"cs", "TODO"},
+				},
+			)
+		}
+	}
+	return http.StatusOK
+}
---
+++ handler/v12/searchrt.go
@@ -0,0 +1,196 @@
+// Copyright 2023 Martin Zimandl <martin.zimandl@gmail.com>
+// Copyright 2023 Tomas Machalek <tomas.machalek@gmail.com>
+// Copyright 2023 Institute of the Czech National Corpus,
+//                Faculty of Arts, Charles University
+//   This file is part of MQUERY.
+//
+//  MQUERY is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  MQUERY is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with MQUERY.  If not, see <https://www.gnu.org/licenses/>.
+
+package v12
+
+import (
+	"encoding/json"
+	"fcs/corpus"
+	"fcs/general"
+	"fcs/query/compiler"
+	"fcs/query/parser/simple"
+	"fcs/rdb"
+	"fcs/results"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (a *FCSSubHandlerV12) translateQuery(
+	corpusName, query string,
+) (compiler.AST, *general.FCSError) {
+	var fcsErr *general.FCSError
+	ast, err := simple.ParseQuery(
+		query,
+		corpus.DefaultLayerType,
+		a.corporaConf.Resources[corpusName].PosAttrs,
+		a.corporaConf.Resources[corpusName].StructureMapping,
+	)
+	if err != nil {
+		fcsErr = &general.FCSError{
+			Code:    general.CodeQuerySyntaxError,
+			Ident:   query,
+			Message: "Invalid query syntax",
+		}
+	}
+	return ast, fcsErr
+}
+
+func (a *FCSSubHandlerV12) searchRetrieve(ctx *gin.Context, fcsResponse *FCSResponse) int {
+	// check if all parameters are supported
+	for key, _ := range ctx.Request.URL.Query() {
+		if err := SearchRetrArg(key).Validate(); err != nil {
+			fcsResponse.General.Error = &general.FCSError{
+				Code:    general.CodeUnsupportedParameter,
+				Ident:   key,
+				Message: err.Error(),
+			}
+			return http.StatusBadRequest
+		}
+	}
+
+	fcsQuery := ctx.Query("query")
+	if len(fcsQuery) == 0 {
+		fcsResponse.General.Error = &general.FCSError{
+			Code:    general.CodeMandatoryParameterNotSupplied,
+			Ident:   "fcs_query",
+			Message: "Mandatory parameter not supplied",
+		}
+		return http.StatusBadRequest
+	}
+
+	corpora := strings.Split(ctx.Query(SearchRetrArgFCSContext.String()), ",")
+	// get searchable corpora and attrs
+	if len(corpora) > 0 {
+		for _, v := range corpora {
+			_, ok := a.corporaConf.Resources[v]
+			if !ok {
+				fcsResponse.General.Error = &general.FCSError{
+					Code:    general.CodeUnsupportedParameterValue,
+					Ident:   SearchRetrArgFCSContext.String(),
+					Message: "Unknown context " + v,
+				}
+				return http.StatusBadRequest
+			}
+		}
+
+	} else {
+		for corpusName, _ := range a.corporaConf.Resources {
+			corpora = append(corpora, corpusName)
+		}
+	}
+	searchAttrs := a.corporaConf.Resources.GetCommonPosAttrNames(corpora...)
+
+	// make searches
+	waits := make([]<-chan *rdb.WorkerResult, len(corpora))
+	for i, corpusName := range corpora {
+
+		ast, fcsErr := a.translateQuery(corpusName, fcsQuery)
+		if fcsErr != nil {
+			fcsResponse.General.Error = fcsErr
+			return http.StatusInternalServerError
+		}
+
+		query := ast.Generate()
+		if len(ast.Errors()) > 0 {
+			fcsResponse.General.Error = &general.FCSError{
+				Code:    general.CodeQueryCannotProcess,
+				Ident:   SearchRetrArgQuery.String(),
+				Message: ast.Errors()[0].Error(),
+			}
+			return http.StatusInternalServerError
+		}
+		args, err := json.Marshal(rdb.ConcExampleArgs{
+			CorpusPath: a.corporaConf.GetRegistryPath(corpusName),
+			QueryLemma: "",
+			Query:      query,
+			Attrs:      searchAttrs,
+			MaxItems:   10,
+		})
+		if err != nil {
+			fcsResponse.General.Error = &general.FCSError{
+				Code:    general.CodeGeneralSystemError,
+				Ident:   err.Error(),
+				Message: "General system error",
+			}
+			return http.StatusInternalServerError
+		}
+		wait, err := a.radapter.PublishQuery(rdb.Query{
+			Func: "concExample",
+			Args: args,
+		})
+		if err != nil {
+			fcsResponse.General.Error = &general.FCSError{
+				Code:    general.CodeGeneralSystemError,
+				Ident:   err.Error(),
+				Message: "General system error",
+			}
+			return http.StatusInternalServerError
+		}
+		waits[i] = wait
+	}
+
+	// gather results
+	results := make([]results.ConcExample, len(corpora))
+	for i, wait := range waits {
+		rawResult := <-wait
+		result, err := rdb.DeserializeConcExampleResult(rawResult)
+		if err != nil {
+			fcsResponse.General.Error = &general.FCSError{
+				Code:    general.CodeGeneralSystemError,
+				Ident:   err.Error(),
+				Message: "General system error",
+			}
+			return http.StatusInternalServerError
+		}
+		if err := result.Err(); err != nil {
+			fcsResponse.General.Error = &general.FCSError{
+				Code:    general.CodeGeneralSystemError,
+				Ident:   err.Error(),
+				Message: "General system error",
+			}
+			return http.StatusInternalServerError
+		}
+		results[i] = result
+	}
+
+	// transform results
+	fcsResponse.SearchRetrieve.Results = make([]FCSSearchRow, 0, 100)
+	for i, r := range results {
+		for _, l := range r.Lines {
+			row := FCSSearchRow{
+				Position: len(fcsResponse.SearchRetrieve.Results) + 1,
+				PID:      corpora[i],
+				Web:      "TODO",
+				Ref:      "TODO",
+			}
+			for _, t := range l.Text {
+				token := Token{
+					Text: t.Word,
+					Hit:  t.Strong,
+				}
+				row.Tokens = append(row.Tokens, token)
+
+			}
+			fcsResponse.SearchRetrieve.Results = append(fcsResponse.SearchRetrieve.Results, row)
+		}
+	}
+	return http.StatusOK
+}
